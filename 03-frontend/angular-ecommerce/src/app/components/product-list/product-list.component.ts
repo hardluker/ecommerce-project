a207d43e74d976b836e265def54714317716588f
Donald Azevedo <dba9514@gmail.com>
2024-02-12 16:01:37 -0500

    Adding additional comments for clarity.

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -8,12 +8,19 @@ import { Product } from '../../common/product';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  //Initializing an array to contain the "list" of products
   products: Product[] = [];
+
+  //Constructor to initialze a product service.
   constructor(private productService: ProductService) {}
 
+  //Run the listProducts methon on post Construct
   ngOnInit(): void {
     this.listProducts();
   }
+
+  //This method calls the service to return the stream of data.
+  //The data is received and the array is populated with this data.
   listProducts() {
     this.productService.getProductList().subscribe((data) => {
       this.products = data;
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,14 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
 
+//This allows for dependency injection
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  //base url for perform http methods on.
   private baseUrl = 'http://localhost:8080/api/products';
 
+  //Constructor to initialize the HttpClient
   constructor(private httpClient: HttpClient) {}
 
+  //This method performs the GET request and then
+  //maps the response to an array. Automagically parsing the json!
   getProductList(): Observable<Product[]> {
     return this.httpClient
       .get<GetResponse>(this.baseUrl)
@@ -18,6 +23,7 @@ export class ProductService {
   }
 }
 
+//This is used to define the structure of the response expected.
 interface GetResponse {
   _embedded: {
     products: Product[];
