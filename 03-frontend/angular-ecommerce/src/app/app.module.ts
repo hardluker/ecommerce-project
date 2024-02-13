bed166b85ffdf213dc343c83a040da0126ef676d
Donald Azevedo <dba9514@gmail.com>
2024-02-13 14:02:12 -0500

    Added route to the app.module.ts

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,6 +13,15 @@ import {
   withFetch,
 } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
