7ab74da7b2c9bfb076a3c5e481331ac770716973
Donald Azevedo <dba9514@gmail.com>
2024-02-13 08:28:33 -0500

    misc updates and running some tests

--- 02-backend/spring-boot-ecommerce/src/main/java/com/dba9514/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/dba9514/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,6 @@ package com.dba9514.ecommerce.dao;
 
 import com.dba9514.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
