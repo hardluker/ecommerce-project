e5cc0d70f1accda38390d1fd95a81755c05ac808
hardluker <dba9514@gmail.com>
2024-02-14 18:27:53 -0500

    Added the ID to the Product class.

--- 03-frontend/angular-ecommerce/src/app/common/product.ts
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,6 +1,7 @@
 //Product class for containing the data received from the backend.
 export class Product {
   constructor(
+    public id: number,
     public sku: string,
     public name: string,
     public description: string,
