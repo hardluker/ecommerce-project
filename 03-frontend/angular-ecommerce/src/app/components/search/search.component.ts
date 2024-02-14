a0e6abdf0424ae63ceeb73b03c99bad4ec42eade
Donald Azevedo <dba9514@gmail.com>
2024-02-14 15:53:53 -0500

    Defined the doSearch method for utilizing the router.

--- 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit() {}
+
+  doSearch(value: string) {
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
 }
