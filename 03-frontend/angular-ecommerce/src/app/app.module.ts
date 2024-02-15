a9567f6e87046f575a74e352f5bf3ef8c1ee87af
Donald Azevedo <dba9514@gmail.com>
2024-02-15 14:53:27 -0500

    Installed ng-bootstrap for pagination support.

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 //These are the routes for displaying information based on certain conditions within certain components.
 const routes: Routes = [
   { path: 'products/:id', component: ProductDetailsComponent },
@@ -42,6 +44,7 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [
     provideClientHydration(),
