6660f0b00cf96b9bb2cfa0a25a1df1ab32e06752
hardluker <dba9514@gmail.com>
2024-02-14 19:03:25 -0500

    setup the product-details component to get a product from the service, post construct.
    I still need to update the product service to actually retrieve this from the backend.

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
+  product!: Product;
 
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  handleProductDetails() {
+    // get the "id" param string. convert string to a number using the + symbol.
+    const productId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(productId).subscribe((data) => {
+      this.product = data;
+    });
+  }
 }
