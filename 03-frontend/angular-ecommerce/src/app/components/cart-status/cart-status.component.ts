d56155d90465aac36954a5a6ea4a30733af40fe2
hardluker <dba9514@gmail.com>
2024-02-16 10:26:02 -0500

    Subscribing to the cartService info and displaying it in the html.

--- 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
-  styleUrl: './cart-status.component.css'
+  styleUrl: './cart-status.component.css',
 })
 export class CartStatusComponent {
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    //Subscribe for cart service events
+
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }
